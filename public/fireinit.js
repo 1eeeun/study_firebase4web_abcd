d78636832e4fc1339c4efde57d44f4faa91d687b
denlyou <denlyou85@gmail.com>
2016-08-03 09:54:55 +0900

    file mod

--- public/fireinit.js
+++
@@ -1,8 +0,0 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyC0VMya-6QGWcvKf5DE93U1V82Zp_yb9_4",
-  authDomain: "hellofirebase4web.firebaseapp.com",
-  databaseURL: "https://hellofirebase4web.firebaseio.com",
-  storageBucket: "hellofirebase4web.appspot.com",
-};
-firebase.initializeApp(config);
